afd60798870e98a8bdb10640e0c157a0619cb319
georg <georg@mihatsch.cc>
2021-02-03 20:58:42 +0100

    change to Potentiometer

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 # ESP32ZwiftSteer
 
 
-#IN PROGRESS!  
+![](steering.jpg)
 
 An ESP32 based, battery powered Zwift Steering Contoller.
-Use an MH-ET LIVE MiniKit for ESP32 and and IMU to steer
+Use an MH-ET LIVE MiniKit for ESP32 a Potentiometer and the 3D Printed parts from https://github.com/kwakeham/zwift-steerer/
+(watch his Video for build instructions: https://www.youtube.com/watch?v=sgZ5960KFTI)
 
 thx to petereverett for his work
 https://sourceforge.net/projects/zwift-steering-esp32/
 
 
+thx to Keith Wakeham for his 3D printed parts
+https://github.com/kwakeham/zwift-steerer/
+
+
 ## BOM
 - [MH-ET LIVE MiniKit for ESP32](https://de.aliexpress.com/item/32814642296.html) x 1   
-- [Adafruit 9-DOF Absolute Orientation IMU Fusion Breakout - BNO055](https://www.adafruit.com/product/4646) x 1
+- [Potentiometer](https://www.adafruit.com/product/4646) x 1
 - [power saving circuit](https://randomnerdtutorials.com/power-saving-latching-circuit/) x 1
-- [Batteriemanagment]() x 1 
+- [3D Printed Parts -> https://github.com/kwakeham/zwift-steerer/](https://github.com/kwakeham/zwift-steerer/)
+- [Batteriemanagment](https://de.aliexpress.com/item/32798858483.html) x 1 
+
+
+## setup
+adapt the values (MAX_ADC_LEFT/MAX_ADC_RIGHT) in the code to your setup 
+
+- enable DEBUG (#define DEBUG)
+- steer full left/full right -> note ADC value and enter the values next to #defines 
--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,130 @@
 /*
-
-//Zwift steering demo code
-//Takes ADC reading from pin 32 and converts to an angle between -40 and +40 and transmits to Zwift via BLE
-
-//Based on BLE Arduino for ESP32 examples (Kolban et al.)
-//Keith Wakeham's explanation https://www.youtube.com/watch?v=BPVFjz5zD4g
-//Andy's demo code: https://github.com/fiveohhh/zwift-steerer/
-
-//Code written in VSCode with PlatformIO for a Lolin32 Lite
-//Should work in Arduino IDE if the #include <Arduino.h> is removed
-
-//Tested using Zwift on Android (Galaxy A5 2017)
-
- * Copyright 2020 Peter Everett
- * v1.0 Sep 2020 - Initial version
- * 
- * This work is licensed the GNU General Public License v3 (GPL-3)
-
+    Zwift Steering 
 */
 
+//Libraries
 
 #include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
-#include "imu.h"
 #include <Ticker.h>
 
 
-#define STEERING_DEVICE_UUID "347b0001-7635-408b-8918-8ff3949ce592"
-#define STEERING_ANGLE_CHAR_UUID "347b0030-7635-408b-8918-8ff3949ce592"     //notify
-/*
-//These charateristics are present on the Sterzo but aren't necessary for communication with Zwift
-#define STEERING_POWER_CHAR_UUID "347b0012-7635-408b-8918-8ff3949ce592"     //write
-#define STEERING_UNKNOWN2_CHAR_UUID "347b0013-7635-408b-8918-8ff3949ce592"  //value 0xFF, read
-#define STEERING_UNKNOWN3_CHAR_UUID "347b0014-7635-408b-8918-8ff3949ce592"  //value 0xFF, notify
-#define STEERING_UNKNOWN4_CHAR_UUID "347b0019-7635-408b-8918-8ff3949ce592"  //value x0FF, read
+/* adapt this values to your setup
+- enable DEBUG (#define DEBUG)
+- steer full left/full right -> note ADC value and enter hier 
 */
-#define STEERING_RX_CHAR_UUID "347b0031-7635-408b-8918-8ff3949ce592"  //write
-#define STEERING_TX_CHAR_UUID "347b0032-7635-408b-8918-8ff3949ce592"  //indicate
+#define MAX_ADC_LEFT 1850
+#define MAX_ADC_RIGHT 2900
 
+Ticker watchDOG;
+#define watchdogMAXCounter 60*15 // 60minuten
+static uint32_t watchdogCounter = watchdogMAXCounter;
 #define POWERLATCH 23
 #define LED 19
 
-Ticker watchDOG;
-#define watchdogMAXCounter 15 * 60 // 60minuten
-static uint32_t watchdogCounter = watchdogMAXCounter;
+//DEBUG
+#define DEBUG
 
-int old_angle=0;
+//BLE Definitions
+#define STEERING_DEVICE_UUID "347b0001-7635-408b-8918-8ff3949ce592"
+#define STEERING_ANGLE_CHAR_UUID "347b0030-7635-408b-8918-8ff3949ce592" //notify
+#define STEERING_RX_CHAR_UUID "347b0031-7635-408b-8918-8ff3949ce592"    //write
+#define STEERING_TX_CHAR_UUID "347b0032-7635-408b-8918-8ff3949ce592"    //indicate
+
+#define POT 32 // Joystick Xaxis to GPIO32
+
+//Angle calculation parametres
+#define MAX_ADC_RESOLUTION 4095 //ESP32 ADC is 12bit
+#define MAX_STEER_ANGLE 35
+#define ZERO_FLOOR 4
 
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 bool auth = false;
 
-double pitch, roll;
-double r_rand = 180 / PI;
-
 float angle = 20;
+float old_angle = 20;
+//Sterzo stuff
 int FF = 0xFF;
 uint8_t authChallenge[4] = {0x03, 0x10, 0xff, 0xff};
 uint8_t authSuccess[3] = {0x03, 0x11, 0xff};
 
-BLEServer* pServer = NULL;
-BLECharacteristic* pAngle = NULL;
-/*
-//These charateristics are present on the Sterzo but aren't necessary for communication with Zwift
-BLECharacteristic* pPwr = NULL;
-BLECharacteristic* pU2 = NULL;
-BLECharacteristic* pU3 = NULL;
-BLECharacteristic* pU4 = NULL;
-*/
-BLECharacteristic* pRx = NULL;
-BLECharacteristic* pTx = NULL;
-
-
-//BLEAdvertisementData advert;
-//BLEAdvertisementData scan_response;
+BLEServer *pServer = NULL;
+BLECharacteristic *pAngle = NULL;
+BLECharacteristic *pRx = NULL;
+BLECharacteristic *pTx = NULL;
 BLEAdvertising *pAdvertising;
 
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
+//Server Callbacks
+class MyServerCallbacks : public BLEServerCallbacks
+{
+    void onConnect(BLEServer *pServer)
+    {
+        deviceConnected = true;
+        BLEDevice::startAdvertising();
+        
     };
 
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
+    void onDisconnect(BLEServer *pServer)
+    {
+        deviceConnected = false;
     }
 };
 
+//Characteristic Callbacks
+class MyCharacteristicCallbacks : public BLECharacteristicCallbacks
+{
+
+    void onRead(BLECharacteristic *pRx)
+    {
+
+    }
+
+    void onWrite(BLECharacteristic *pRx){
+
+        std::string rxValue = pRx->getValue();
+        
+        if(rxValue.length() == 4){
+          delay(250);
+          pTx->setValue(authSuccess,3);
+          pTx->indicate();
+          auth = true;
+          #ifdef DEBUG
+          Serial.println("Auth Success!");
+          #endif
+        }
+    }
+};
+
+//Joystick read angle from axis
+float readAngle()
+{
+    int potVal = analogRead(POT);
+    
+    // angle = map(potVal,0,MAX_ADC_RESOLUTION,-35,35); //Mapping function
+    angle = map(potVal,MAX_ADC_LEFT,2900,MAX_STEER_ANGLE,-1*MAX_STEER_ANGLE); //Mapping function
+    
+    
+    // kwakeham style:
+    
+    // angle = (((potVal) / (float)MAX_ADC_RESOLUTION) * (MAX_STEER_ANGLE * 2)) - MAX_STEER_ANGLE;
+    
+    if (fabsf(angle) < ZERO_FLOOR){
+        angle = 0;
+    }
+   
+    #ifdef DEBUG
+    Serial.print("ADC:");
+    Serial.print(potVal);
+    Serial.print("\tangle:");
+    Serial.println(angle);
+    #endif
+
+    return angle;
+}
 
 
 void setupPWR(){
@@ -110,137 +146,134 @@ void fct_powerdown(){
 
 void fct_Watchdog() {
   watchdogCounter--;
-  Serial.println("Watchdog COunter:");
+  Serial.print("Watchdog Counter:");
   Serial.println(watchdogCounter);
   if (watchdogCounter < 1) {
     fct_powerdown();
   }
 }
 
-void setup() {
-  setupPWR();
- 
-  //setup pins for Pot
- 
+//Arduino setup
+void setup()
+{
+    setupPWR();
+    watchDOG.attach(1, fct_Watchdog);
+    //Serial Debug
+    Serial.begin(115200);
+    
+    pinMode(POT, INPUT);    // GPIO32 will be => Xaxis on Joystick
+    
+    
+    //Setup BLE
+    #ifdef DEBUG
+    Serial.println("Creating BLE server...");
+    #endif
+    BLEDevice::init("STEERING");
 
-  Serial.begin(115200);
-  imu_setup();
-  watchDOG.attach(1, fct_Watchdog);
-  //Setup BLE
-  Serial.println("Creating BLE server...");
-  BLEDevice::init("STEERING");
+    // Create the BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
 
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+    // Create the BLE Service
+    #ifdef DEBUG
+    Serial.println("Define service...");
+    #endif
+    BLEService *pService = pServer->createService(STEERING_DEVICE_UUID);
 
-  // Create the BLE Service
-  Serial.println("Define service...");
-  BLEService *pService = pServer->createService(STEERING_DEVICE_UUID);
+    // Create BLE Characteristics
+    #ifdef DEBUG
+    Serial.println("Define characteristics");
+    #endif
+    pTx = pService->createCharacteristic(STEERING_TX_CHAR_UUID, BLECharacteristic::PROPERTY_INDICATE | BLECharacteristic::PROPERTY_READ);
+    pTx->addDescriptor(new BLE2902());
+    
+    pRx = pService->createCharacteristic(STEERING_RX_CHAR_UUID, BLECharacteristic::PROPERTY_WRITE);
+    pRx->addDescriptor(new BLE2902());
+    pRx->setCallbacks(new MyCharacteristicCallbacks());
 
-  // Create BLE Characteristics
-  Serial.println("Define characteristics");
-  //The Sterzo includes all of these characteristics, but you only need the Tx and Rx (for the handshaking) and the steerer angle sensor
-  /*
-  pPwr = pService->createCharacteristic(STEERING_POWER_CHAR_UUID,BLECharacteristic::PROPERTY_WRITE);
-  pPwr->addDescriptor(new BLE2902());
-  pU2 = pService->createCharacteristic(STEERING_UNKNOWN2_CHAR_UUID,BLECharacteristic::PROPERTY_READ);
-  pU2->addDescriptor(new BLE2902());
-  pU3 = pService->createCharacteristic(STEERING_UNKNOWN3_CHAR_UUID,BLECharacteristic::PROPERTY_NOTIFY);
-  pU3->addDescriptor(new BLE2902());
-  pU4 = pService->createCharacteristic(STEERING_UNKNOWN4_CHAR_UUID,BLECharacteristic::PROPERTY_READ);
-  pU4->addDescriptor(new BLE2902());*/
+    pAngle = pService->createCharacteristic(STEERING_ANGLE_CHAR_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+    pAngle->addDescriptor(new BLE2902());
 
-  pTx = pService->createCharacteristic(STEERING_TX_CHAR_UUID,BLECharacteristic::PROPERTY_INDICATE | BLECharacteristic::PROPERTY_READ);
-  pTx->addDescriptor(new BLE2902());
-  pRx = pService->createCharacteristic(STEERING_RX_CHAR_UUID,BLECharacteristic::PROPERTY_WRITE);
-  pRx->addDescriptor(new BLE2902());
-  pAngle = pService->createCharacteristic(STEERING_ANGLE_CHAR_UUID,BLECharacteristic::PROPERTY_NOTIFY);
-  pAngle->addDescriptor(new BLE2902());
+    // Start the service
+    #ifdef DEBUG
+    Serial.println("Staring BLE service...");
+    #endif
+    pService->start();
 
-  // Start the service
-  Serial.println("Staring BLE service...");
-  pService->start();
-
-  // Start advertising
-  // Zwift only shows the steering button when the service is advertised
-  Serial.println("Define the advertiser...");
-  pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->setScanResponse(true);  
-  pAdvertising->addServiceUUID(STEERING_DEVICE_UUID);
-  pAdvertising->setMinPreferred(0x06);  // set value to 0x00 to not advertise this parameter
-  Serial.println("Starting advertiser...");
-  BLEDevice::startAdvertising();
-  Serial.println("Waiting a client connection to notify...");
+    // Start advertising
+    #ifdef DEBUG
+    Serial.println("Define the advertiser...");
+    #endif
+    pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->setScanResponse(true);
+    pAdvertising->addServiceUUID(STEERING_DEVICE_UUID);
+    pAdvertising->setMinPreferred(0x06); // set value to 0x00 to not advertise this parameter
+    #ifdef DEBUG
+    Serial.println("Starting advertiser...");
+    #endif
+    BLEDevice::startAdvertising();
+    #ifdef DEBUG
+    Serial.println("Waiting a client connection to notify...");
+    #endif
 }
 
-// void loop() {
-//       angle = getCurrentAngle();
-//       Serial.print("Transmitting angle: ");
-//       Serial.println(angle);
-      
-//   delay(100);
-// }
+//Arduino loop
+void loop()
+{
 
-void loop() {
-  if (deviceConnected) {
-    if (auth) {
-      //Connected to Zwift so read the potentiometer and start transmitting the angle
-      angle = getCurrentAngle();
-      if(abs(angle-old_angle)>2){
-        watchdogCounter=watchdogMAXCounter;
-      }
-      old_angle=angle;
-      Serial.print("Transmitting angle: ");
-      Serial.println(angle);
-      pAngle->setValue(angle);
-      pAngle->notify();
-      delay(500);
-    } else {
-      //Not connected to Zwift so start the connectin process
-      pTx->setValue(FF);
-      pTx->indicate();
-      //Do the handshaking
-      std::string rxValue = pRx->getValue();
-      if (rxValue.length() == 0) {
-        Serial.println("No data received");
-        delay(250);
-      } else {
-        Serial.print("Handshaking....");
-        if (rxValue[0] == 0x03 && rxValue[1] == 0x10) {
-          delay(250);
-          //send 0x0310FFFF (the last two octets can be anything)
-          pTx->setValue(authChallenge,4);
-          pTx->indicate();
-          //Zwift will now send 4 bytes as a response, which start with 0x3111
-          //We don't really care what it is as long as we get a response
-          delay(250);
-          rxValue = pRx->getValue();
-          if (rxValue.length() == 4) {
-            //connected, so send 0x0311ff
-            delay(250);
-            pTx->setValue(authSuccess,3);
-            pTx->indicate();
-            auth = true;
-            Serial.println("Success!");
+    if (deviceConnected)
+    {
+        
+        if(auth){
+          angle = readAngle();
+          if(abs(angle-old_angle)>2){
+                watchdogCounter=watchdogMAXCounter;
           }
+          Serial.print("angle");
+          Serial.println(angle);
+          old_angle=angle;
+          pAngle->setValue(angle);
+          pAngle->notify();
+          #ifdef DEBUG
+          Serial.print("TX Angle: ");
+          Serial.println(angle);
+          #endif
+          delay(250);
+        } else {
+          #ifdef DEBUG
+          Serial.println("Auth Challenging");
+          #endif
+          pTx->setValue(authChallenge, 4);
+          pTx->indicate();
+          delay(250);
+        
         }
-      }
     }
-      delay(50);  //small delay so BLW stack doesn't get overloaded
-    }
-    // disconnecting
-    if (!deviceConnected && oldDeviceConnected) {
-        delay(300); // give the bluetooth stack the chance to get things ready
+
+    //Advertising
+    if (!deviceConnected && oldDeviceConnected)
+    {
+        delay(300);                  // give the bluetooth stack the chance to get things ready
         pServer->startAdvertising(); // restart advertising
+        #ifdef DEBUG
         Serial.println("Nothing connected, start advertising");
+        #endif
         oldDeviceConnected = deviceConnected;
     }
-    // connecting
-    if (deviceConnected && !oldDeviceConnected) {
+   
+    //Connecting
+    if (deviceConnected && !oldDeviceConnected)
+    {
         oldDeviceConnected = deviceConnected;
+        #ifdef DEBUG
         Serial.println("Connecting...");
+        #endif
     }
-    if (!deviceConnected) {
+
+    if (!deviceConnected)
+    {
+        #ifdef DEBUG
+        readAngle();
+        #endif
     }
 }
